f46242e42a20ebf663940b01957162a831820664
tominari <tominari@hitokuse.com>
2017-01-12 18:54:28 +0900

    https response

--- trigger.js
+++ trigger.js
@@ -1,22 +1,41 @@
+
+var https = require('https');
 var aws = require('aws-sdk');
 var s3 = new aws.S3();
 
 exports.handler = function(event, context) {
-   var bucket = event.Records[0].s3.bucket.name;
-   var key = event.Records[0].s3.object.key;
-   s3.getObject({
-       Bucket:bucket,
-       Key:key
-   },
-    function(err,data) {
-        if (err) {
-               console.log('error getting object ' + key + ' from bucket ' + bucket + 
-                   '. Make sure they exist and your bucket is in the same region as this function.');
-               context.done('error','error getting file'+err);
-            }
-            else {
-                console.log(String(data.Body));
-            }
-        }
-   );
-};
+
+  var key = event.Records[0].s3.object.key;
+  var url = "" + key
+
+  console.log('start request to ' + url);
+    
+  https.get(url, function(res) {
+    
+    console.log("Got response: " + res.statusCode);
+    
+    var body = '';
+    
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+    
+    res.on('end', function() {
+      console.log(body);
+      context.succeed();
+    });
+    
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+    context.done(null, 'FAILURE');
+  
+  });
+
+  console.log('end request to ' + url);
+}
+
+
+
+
+
+
